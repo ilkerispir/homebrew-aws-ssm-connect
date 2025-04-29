1a3d865deb13e6da3e05e0cf8b1fcfa7cc075bd7
Ilker Ispir <ilker@ispir.dev>
2025-04-30 00:09:24 +1000

    aws-ssm-tunnel 0.0.2 (new formula)

--- Formula/aws-ssm-tunnel.rb
+++ Formula/aws-ssm-tunnel.rb
@@ -1,16 +1,18 @@
 class AwsSsmTunnel < Formula
   desc "CLI tool to tunnel into RDS & ElastiCache over SSM-managed EC2 instances"
   homepage "https://github.com/ilkerispir/aws-ssm-tunnel"
-  url "https://github.com/ilkerispir/aws-ssm-tunnel/releases/download/0.0.2/aws-ssm-tunnel-v0.0.2.tar.gz"
-  
-  sha256 "54b87923ea68280ab167eb6f80d70a4b9927e8fdb2ad83a06d26a901dc345b32"
+  url "https://github.com/ilkerispir/aws-ssm-tunnel/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "4ba6dbd23d33968a17eceb5c4792a1fd396ff6978b57c195e5758e47328d0863"
+  license "Apache-2.0"
   version "0.0.2"
 
+  depends_on "go" => :build
+
   def install
-    bin.install "aws-ssm-tunnel"
+    system "go", "build", *std_go_args(output: bin/"aws-ssm-tunnel")
   end
 
   test do
-    system "#{bin}/aws-ssm-tunnel", "--help"
+    assert_match "Usage", shell_output("#{bin}/aws-ssm-tunnel --help")
   end
 end
