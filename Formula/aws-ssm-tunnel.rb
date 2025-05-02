c7b02f62fd09a345e832f5af841fdbca9d3a3403
Ilker Ispir <ilker@ispir.dev>
2025-05-02 19:15:58 +1000

    fix: updated version

--- Formula/aws-ssm-tunnel.rb
+++ Formula/aws-ssm-tunnel.rb
@@ -5,14 +5,14 @@ class AwsSsmTunnel < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/ilkerispir/aws-ssm-tunnel/releases/download/v0.0.2/aws-ssm-tunnel-darwin-amd64.tar.gz"
-      sha256 "a0d57f9b4743c42877e07f9caa78f1d4d2e7859528927e8bc906b097b77f3033"
+      sha256 "b20d9adaf8eea875fd6384742bcfef815ef74e8f064c05e2d57289a21551d42a"
     elsif Hardware::CPU.arm?
       url "https://github.com/ilkerispir/aws-ssm-tunnel/releases/download/v0.0.2/aws-ssm-tunnel-darwin-arm64.tar.gz"
-      sha256 "4d34992e6ff19fbf1e4ca1c7aa2572205cd8a85e08ed3b7215cf69da84eb5b75"
+      sha256 "1b7e5cfbaaef887fcccf619a285a4b0be8de64cfbfe3175140c8dc4b10c5c4cf"
     end
   end
 
-  version "0.0.2"
+  version "0.0.3"
 
   def install
     bin.install "aws-ssm-tunnel"
