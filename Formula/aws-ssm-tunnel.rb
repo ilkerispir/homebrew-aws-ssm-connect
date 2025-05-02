a71e872ec1cc73e4fa2aaa2f91e0cabd0e9b8c5b
Ilker Ispir <ilker@ispir.dev>
2025-05-02 18:49:33 +1000

    updated brew file

--- Formula/aws-ssm-tunnel.rb
+++ Formula/aws-ssm-tunnel.rb
@@ -5,10 +5,10 @@ class AwsSsmTunnel < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/ilkerispir/aws-ssm-tunnel/releases/download/v0.0.2/aws-ssm-tunnel-darwin-amd64.tar.gz"
-      sha256 "4bc284301ba013af5ce5a8203408250c71e924343d3264a52de8a769fa49840b"
+      sha256 "a0d57f9b4743c42877e07f9caa78f1d4d2e7859528927e8bc906b097b77f3033"
     elsif Hardware::CPU.arm?
       url "https://github.com/ilkerispir/aws-ssm-tunnel/releases/download/v0.0.2/aws-ssm-tunnel-darwin-arm64.tar.gz"
-      sha256 "2766c57ee488a977515e206108a465449779b4d997a8e9944b4205dbf3adae22"
+      sha256 "4d34992e6ff19fbf1e4ca1c7aa2572205cd8a85e08ed3b7215cf69da84eb5b75"
     end
   end
 
