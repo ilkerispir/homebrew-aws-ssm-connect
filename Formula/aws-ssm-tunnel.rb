16edebca9be1e64963fd70b81a569fcd4b2b22fc
Ilker Ispir <ilker@ispir.dev>
2025-05-02 18:38:15 +1000

    updated brew file

--- Formula/aws-ssm-tunnel.rb
+++ Formula/aws-ssm-tunnel.rb
@@ -1,18 +1,24 @@
 class AwsSsmTunnel < Formula
   desc "CLI tool to tunnel into RDS & ElastiCache over SSM-managed EC2 instances"
   homepage "https://github.com/ilkerispir/aws-ssm-tunnel"
-  url "https://github.com/ilkerispir/aws-ssm-tunnel/archive/refs/tags/0.0.2.tar.gz"
-  sha256 "4ba6dbd23d33968a17eceb5c4792a1fd396ff6978b57c195e5758e47328d0863"
-  license "Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/ilkerispir/aws-ssm-tunnel/releases/download/v0.0.2/aws-ssm-tunnel-darwin-amd64.tar.gz"
+      sha256 "4bc284301ba013af5ce5a8203408250c71e924343d3264a52de8a769fa49840b"
+    elsif Hardware::CPU.arm?
+      url "hhttps://github.com/ilkerispir/aws-ssm-tunnel/releases/download/v0.0.2/aws-ssm-tunnel-darwin-arm64.tar.gz"
+      sha256 "2766c57ee488a977515e206108a465449779b4d997a8e9944b4205dbf3adae22"
+    end
+  end
+
   version "0.0.2"
 
-  depends_on "go" => :build
-
   def install
-    system "go", "build", *std_go_args(output: bin/"aws-ssm-tunnel")
+    bin.install "aws-ssm-tunnel"
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/aws-ssm-tunnel --help")
+    system "#{bin}/aws-ssm-tunnel", "--help"
   end
 end
